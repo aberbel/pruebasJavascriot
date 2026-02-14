b08e5e8ea479967c86775c990eef5d15aca1905c
aberbel <aberbel@gmail.com>
2026-02-14 20:47:17 +0100

    primer test- fizzbuzz es una función

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Prueba de Test con IA
+## Hecho con Vitest
+<pre>
+npm install -D vitest
+</pre>
+<pre>
+npm init -Y : Modifica el json
+</pre>
+<pre>
+npm install standard -D: reglas de linter, malas prácticas
+</pre>
+
+To remove a folder and all files in the folder recursively:
+<pre>
+git rm -r --cached folder
+</pre>
+<pre>
+{
+  "scripts": {
+    "test": "vitest"
+  }
+}
+</pre>
+- Crear carpeta test
+- Crear primer test: fizzbuzz.test.js
---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest';
+
+/*
+Escribe una función que al pasarle un número, devuelva:
+- "fizz" si el número es múltiplo de 3
+- "buzz" si el número es múltiplo de 5
+- "fizzbuzz" si el número es múltiplo de 3 y de 5
+- El número, en caso contrario
+*/
+
+const fizzbuzz = (num) => {
+
+};
+describe('fizzbuzz', () => {
+  it('debería ser una función', () => {
+    expect(typeof fizzbuzz).toBe('function');
+})
+});
